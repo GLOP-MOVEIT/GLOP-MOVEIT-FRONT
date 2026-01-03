02a316213674f198480481fdb04496dda098a0ff
AymericJak <aymericjakpro@gmail.com>
2026-01-03 20:24:26 +0100

    refactor: navbar & footer

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+import { useUserStore } from '@/stores/user'
+import LanguageSwitcher from '@/components/LanguageSwitcher.vue'
+
+const router = useRouter()
+const userStore = useUserStore()
+const { t } = useI18n()
+
+const drawer = ref(false)
+
+const menuItems = computed(() => [{ title: t('nav.home'), icon: 'mdi-home', to: '/' }])
+
+const logout = async () => {
+  await userStore.logout()
+  router.push('/login')
+}
+</script>
+
+<template>
+  <v-app-bar color="primary" :elevation="2" app>
+    <v-app-bar-nav-icon
+      variant="text"
+      @click.stop="drawer = !drawer"
+      class="d-md-none"
+    ></v-app-bar-nav-icon>
+    <v-toolbar-title class="text-h5 font-weight-bold d-flex align-center">
+      <router-link to="/" class="app-title">
+        <v-icon icon="mdi-medal" class="mr-2"></v-icon>
+        CiblOrgaSport - {{ t('nav.home') }}
+      </router-link>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <div class="d-none d-md-flex app-nav-spacer"></div>
+    <LanguageSwitcher class="ml-2" />
+    <v-menu v-if="userStore.isAuthenticated">
+      <template v-slot:activator="{ props }">
+        <v-btn icon v-bind="props">
+          <v-icon>mdi-account-circle</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item to="/profil">
+          <!-- TODO: Mon profil & paramètres : modification/ préférences etc-->
+          <v-list-item-title>{{ t('nav.profile') }}</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>{{ t('nav.settings') }}</v-list-item-title>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item @click="logout">
+          <v-list-item-title>{{ t('nav.logout') }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <v-btn v-else to="/login" variant="text">
+      <v-icon icon="mdi-login" class="mr-2"></v-icon>
+      {{ t('nav.login') }}
+    </v-btn>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" temporary app>
+    <v-list>
+      <v-list-item
+        prepend-icon="mdi-medal"
+        title="CiblOrgaSport"
+        :subtitle="t('nav.menu')"
+      ></v-list-item>
+    </v-list>
+    <v-divider></v-divider>
+    <v-list density="compact" nav>
+      <v-list-item
+        v-for="item in menuItems"
+        :key="item.to"
+        :prepend-icon="item.icon"
+        :title="item.title"
+        :to="item.to"
+      ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<style scoped>
+.app-title {
+  display: inline-flex;
+  align-items: center;
+  font-weight: 700;
+  letter-spacing: 0.2px;
+  text-decoration: none;
+  color: inherit;
+}
+
+.app-title:hover {
+  opacity: 0.9;
+}
+
+.app-nav-spacer {
+  width: 16px;
+}
+</style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,156 +1,16 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import { useRouter } from 'vue-router'
-import { useI18n } from 'vue-i18n'
-import { useUserStore } from '@/stores/user'
-import LanguageSwitcher from '@/components/LanguageSwitcher.vue'
-
-const router = useRouter()
-const userStore = useUserStore()
-const drawer = ref(false)
-const { t } = useI18n()
-
-const menuItems = computed(() => [{ title: t('nav.home'), icon: 'mdi-home', to: '/' }])
-
-const currentYear = computed(() => new Date().getFullYear())
-
-const logout = async () => {
-  await userStore.logout()
-  router.push('/login')
-}
-
+import AppNavbar from '@/components/AppNavbar.vue'
+import AppFooter from '@/components/AppFooter.vue'
 </script>
 
 <template>
   <v-app>
-    <v-app-bar 
-      color="primary" 
-      :elevation="2"
-      app
-    >
-      <v-app-bar-nav-icon 
-        variant="text" 
-        @click.stop="drawer = !drawer"
-        class="d-md-none"
-      ></v-app-bar-nav-icon>
-      <v-toolbar-title class="text-h5 font-weight-bold">
-        <v-icon icon="mdi-medal" class="mr-2"></v-icon>
-        CiblOrgaSport
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn 
-        v-for="item in menuItems" 
-        :key="item.to"
-        :to="item.to"
-        variant="text"
-        class="d-none d-md-flex"
-      >
-        <v-icon :icon="item.icon" class="mr-2"></v-icon>
-        {{ item.title }}
-      </v-btn>
-      <LanguageSwitcher class="ml-2" />
-      <v-menu v-if="userStore.isAuthenticated">
-        <template v-slot:activator="{ props }">
-          <v-btn icon v-bind="props">
-            <v-icon>mdi-account-circle</v-icon>
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item to="/profil">
-            <v-list-item-title>{{ t('nav.profile') }}</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title>{{ t('nav.settings') }}</v-list-item-title>
-          </v-list-item>
-          <v-divider></v-divider>
-          <v-list-item @click="logout">
-            <v-list-item-title>{{ t('nav.logout') }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-      <v-btn 
-        v-else
-        to="/login"
-        variant="text"
-      >
-        <v-icon icon="mdi-login" class="mr-2"></v-icon>
-        {{ t('nav.login') }}
-      </v-btn>
-    </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      temporary
-      app
-    >
-      <v-list>
-        <v-list-item
-          prepend-icon="mdi-medal"
-          title="CiblOrgaSport"
-          :subtitle="t('nav.menu')"
-        ></v-list-item>
-      </v-list>
-      <v-divider></v-divider>
-      <v-list density="compact" nav>
-        <v-list-item
-          v-for="item in menuItems"
-          :key="item.to"
-          :prepend-icon="item.icon"
-          :title="item.title"
-          :to="item.to"
-        ></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <AppNavbar />
     <v-main>
       <v-container fluid class="pa-0">
         <router-view />
       </v-container>
     </v-main>
-    <v-footer 
-      color="grey-lighten-3" 
-      app
-      class="d-flex flex-column flex-md-row align-center"
-    >
-      <v-container>
-        <v-row align="center" justify="space-between">
-          <v-col cols="12" md="6" class="text-center text-md-left">
-            <strong>CiblOrgaSport</strong> © {{ currentYear }} - {{ t('footer.rightsReserved') }}
-          </v-col>
-          <v-col cols="12" md="6" class="text-center text-md-right">
-            <v-btn 
-              variant="text" 
-              size="small" 
-              href="#" 
-              class="mx-1"
-            >
-              {{ t('footer.legal') }}
-            </v-btn>
-            <v-btn 
-              variant="text" 
-              size="small" 
-              href="#" 
-              class="mx-1"
-            >
-              {{ t('footer.privacy') }}
-            </v-btn>
-            <v-btn 
-              variant="text" 
-              size="small" 
-              href="#" 
-              class="mx-1"
-            >
-              {{ t('footer.contact') }}
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-footer>
+    <AppFooter />
   </v-app>
 </template>
-
-
-<style scoped>
-.v-toolbar-title {
-  display: flex;
-  align-items: center;
-}
-</style>
